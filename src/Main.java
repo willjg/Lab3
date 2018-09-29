cd1959c3e08deed94d9fd51761b23e8f911b5d98
willjg <wjguo2@illinois.edu>
2018-09-29 16:31:42 -0500

    Trying to make Git work. JFC.

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,6 @@ public class Main {
 
     public static void main(String[] args) {
         //String text = urlToString("https://www.bls.gov/tus/charts/chart9.txt");
-        System.out.println("Hello, nothing here");
+        System.out.println("Hello, I don't know how to use Git. Send help.");
     }
 }
